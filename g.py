c446fe6966f65787a0383f2b883a032ed6354bea
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-04-16 17:12:29 +0600

    Пофиксил отрисовку

--- g.py
+++ g.py
@@ -15,36 +15,40 @@ import matplotlib.pyplot as plt
 
 matplotlib.use("Agg")
 
-fig = pylab.figure()
-
-ax = fig.gca()
-
-WIDTH = 800
-
-HEIGHT = 800
-
-
-
-
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-# screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Умный муравей")
 clock = pygame.time.Clock()
 
 
 
-FPS = 10
+FPS = 30
 
 env = gym.make("ALE/Breakout-v5")
 
 observation, info = env.reset()
 
+def get_image(observ):
+    fig = pylab.figure()
+    ax = fig.gca()
+    ax.imshow(observ)
+    canvas = agg.FigureCanvasAgg(fig)
+    canvas.draw()
+    renderer = canvas.get_renderer()
+    raw_data = renderer.tostring_rgb()
+    size = canvas.get_width_height()
+    surf = pygame.image.fromstring(raw_data, size, "RGB")
+    return surf
+
 running = True
 while running:
 
     clock.tick(FPS)
-
+    surf = get_image(observation)
+    screen.blit(surf, dest = (0,0))
+    observation,reward, terminated, truncated, info = env.step(env.action_space.sample())
+    if terminated:
+        observation, info = env.reset()
     pressed = pygame.key.get_pressed()
 
     for event in pygame.event.get():
@@ -54,15 +58,4 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
-
-    for _ in range(500):
-        observation,reward, terminated, truncated, info = env.step(env.action_space.sample()) 
-        ax.imshow(observation)
-        canvas = agg.FigureCanvasAgg(fig)
-        canvas.draw()
-        renderer = canvas.get_renderer()
-        raw_data = renderer.tostring_rgb()
-        size = canvas.get_width_height()
-        surf = pygame.image.fromstring(raw_data, size, "RGB")
-        screen.blit(surf, (0,0))
-        pygame.display.flip()
+    pygame.display.flip()
