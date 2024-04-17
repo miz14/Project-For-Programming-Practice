1de7e1b7b70537c9782f48675c28824c1c17c7e8
Vlad <vpashenko14@gmail.com>
2024-04-17 18:51:56 +0600

    библотеки и выбор версии breakout-v4

--- breakout/import.py
+++ breakout/import.py
@@ -1,5 +1,6 @@
 import numpy as np
 import gym
+import matplotlib.pyplot as plt
 
 # rgb
 # frameskip=(2, 5) - пока хз, что это
@@ -7,6 +8,11 @@ import gym
 
 env = gym.make('Breakout-v4')
 env.reset()
-action = env.action_space.sample()
-print(env.step(action))
+for i in range(50):
+    action = env.action_space.sample()
+    env.step(action)
+img = env.step(action)[0]
+plt.imshow(img, interpolation='nearest', aspect='auto')
+# plt.imshow(img)
+plt.show()
 
