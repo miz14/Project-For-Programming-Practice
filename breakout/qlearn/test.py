6c05e653cddf6c6832a5525dc83c921388541f77
Vlad <vpashenko14@gmail.com>
2024-04-21 18:44:05 +0600

    Интервалы для моделей

--- breakout/qlearn/test.py
+++ breakout/qlearn/test.py
@@ -1,9 +1,8 @@
-# from ..breakout import Breakout
-
 from breakout import Breakout
 
 from q_learn import Estimator
 import numpy as np
+import math
 
 
 
@@ -23,14 +22,15 @@ estimator = Estimator(0, 50, 6, 3, 'cpu')
 estimator.load("breakout/qlearn/models/trained_model.pth")
 estimators.append(estimator)
 
+n_x = 60
 
 for est in estimators:
 
     rewards = []
-    for i in range(1):
+    for i in range(60):
         state, img, info = env.reset()
         reward_sum = 0
-        for i in range(1000):
+        for i in range(10000):
             action = np.argmax(est.predict(state))
             state, reward, is_done, term, img, info = env.step(action)
             # print(state)
@@ -41,3 +41,13 @@ for est in estimators:
         rewards.append(reward_sum)
 
     print(sorted(rewards))
+
+    a = np.mean(rewards)
+    alpha = 0.05
+    S = np.std(rewards)
+    S_2 = np.var(rewards)
+    n_free = n_x - 1
+    t_student = 2.000
+
+    print(f'Выборочное среднее: {a}; std: {S}; var {S_2}')
+    print(f'Доверительный интервал для выборочного среднего при неизвестной дисперсии: {a - S/math.sqrt(n_free)* t_student}, {a + S / math.sqrt(n_free)* t_student}')
