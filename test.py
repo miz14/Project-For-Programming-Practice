8ec194e802cfcc1449b5d614aa5d8c6091f60f1a
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-04-26 10:50:04 +0600

    ppo

--- test.py
+++ test.py
@@ -1,39 +1,38 @@
-import cv2
-from matplotlib import pyplot as plt
+import random
+import time
+import gymnasium as gym
 import numpy as np
+import torch
+from ppo import Agent, make_env, Args
+from moviepy import *
 
+args = Args
+args.batch_size = int(args.num_envs * args.num_steps)
+args.minibatch_size = int(args.batch_size // args.num_minibatches)
+args.num_iterations = args.total_timesteps // args.batch_size
+run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
 
-def cut_image(img):
-    img_cut = img[31: 195, 8: 152]
-    return img_cut
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.backends.cudnn.deterministic = args.torch_deterministic
 
+device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+# env setup
+envs = gym.vector.SyncVectorEnv(
+    [make_env(args.env_id, i, args.capture_video, run_name) for i in range(args.num_envs)],
+)
+checkpoint = torch.load('models/agent_checkpoint[30.].pth')
 
-def detect_rectangle(image, obj):
-    # image = image[25:, :]
-    # image = image[:, :8]
-    # image = image[:, 151:]
-    image = cut_image(image)
-    if obj == 'ball':
-        image = image[62:158, :]
-    elif obj == 'board':
-        image = image[156:163, :]
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, mask = cv2.threshold(image, 80, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    rectangle_coords = (None, None)
-    for contour in contours:
-        x, y, width, height = cv2.boundingRect(contour)
-        print(x, y)
-        rectangle_coords = (x + width // 2, y + height // 2)
-        plt.imshow(image, cmap='gray')
-        plt.show()
-        break
-    
-    return rectangle_coords
+agent = Agent(envs).to(device)
 
+agent.load_state_dict(checkpoint['agent_state_dict'])
 
-observation = cv2.imread('observation.jpg')
-image_array = np.array(observation)
-print(detect_rectangle(image_array, 'ball'))
-plt.imshow(image_array, cmap='gray')
-plt.show()
+next_obs, _ = envs.reset(seed=args.seed)
+
+next_done = False
+while True:
+    action, logprob, _, value = agent.get_action_and_value(torch.Tensor(next_obs).to(device))
+    next_obs, reward, terminations, truncations, infos = envs.step(action.cpu().numpy())
+
+envs.close()
